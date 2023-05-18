42bb16b8083af4e0aba814e264adb79a27250f95
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-18 09:46:50 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 18 May 2023 12:44:35 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
